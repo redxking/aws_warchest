5bded0f259c124517b832d6f463a09d29bc5c194
Zach Dobrenen <zdobrenen@thedevteam.dev>
2022-09-18 21:01:01 -0700

    working on tf vpc module

--- terraform/configs/networking/01-vpcs/main.tf
+++ terraform/configs/networking/01-vpcs/main.tf
@@ -1,23 +1,47 @@
+locals {
+  tags = merge(
+    { namespace: var.namespace },
+    { environment: var.environment },
+    { region: var.region },
+    { provisioner: var.provisioner }
+  )
+}
+
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.vpc_name
-  cidr = var.vpc_cidr
+  cidr = lookup(var.vpc_cidr, var.region)
+  azs  = lookup(var.vpc_availability_zones, var.region, null)
+  
+  enable_dns_hostnames   = var.vpc_enable_dns_hostnames 
+  enable_vpn_gateway     = var.vpc_enable_vpn_gateway
+  
+  enable_nat_gateway     = var.vpc_enable_nat_gateway
+  single_nat_gateway     = var.vpc_sinlge_nat_gateway
+  one_nat_gateway_per_az = var.vpc_one_nat_gateway_per_az
 
-  azs                 = var.availability_zones
-  database_subnets    = var.database_subnets
-  elasticache_subnets = var.elasticache_subnets
-  intra_subnets       = var.intra_subnets
-  private_subnets     = var.private_subnets 
-  public_subnets      = var.public_subnets
-  redshift_subnets    = var.redshift_subnets
+  database_subnets                = lookup(var.vpc_database_subnet_cidr, var.region, null)
+  database_subnet_suffix          = var.vpc_database_subnet_suffix
+  create_database_subnet_group    = var.vpc_create_database_subnet_group
 
-  enable_nat_gateway = var.enable_nat_gateway
-  enable_vpn_gateway = var.enable_vpn_gateway
+  elasticache_subnets             = lookup(var.vpc_elasticache_subnet_cidr, var.regaion, null)
+  elasticache_subnet_suffix       = var.vpc_elasticache_subnet_suffix
+  create_elasticache_subnet_group = var.vpc_create_elasticache_subnet_group
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  intra_subnets                   = lookup(var.vpc_intra_subnet_cidr, var.region, null)
+  intra_subnet_suffix             = var.vpc_intra_subnet_suffix
+  
+  private_subnets                 = lookup(var.vpc_private_subnet_cidr, var.region, null)
+  private_subnet_suffix           = var.vpc_private_subnet_suffix
+  
+  public_subnets                  = lookup(var.vpc_public_subnet_cidr, var.region, null)
+  public_subnet_suffix            = var.vpc_public_subnet_suffix
+
+  redshift_subnets                = lookup(var.vpc_redshift_subnet_cidr, var.region, null)
+  redshift_subnet_suffix          = var.vpc_redshift_subnet_suffix
+  create_redshift_subnet_group    = var.vpc_create_redshift_subnet_group 
+
+  tags = local.tags 
 }
--- terraform/configs/networking/01-vpcs/variables.tf
+++ terraform/configs/networking/01-vpcs/variables.tf
@@ -1,6 +1,11 @@
 #
 # Common Variables
 #
+variable "namespace" {
+    description = "Namespace to Deploy Resources"
+    type        = string
+}
+
 variable "environment" {
     description = "Environment to Deploy Resources"
     type        = string
@@ -15,20 +20,139 @@ variable "region" {
 # Resource Variables
 #
 variable "vpc_name" {
-    default     = null
     description = "Name of the VPC to create - used for 'Name' tag on all resources (default: <company>-<environment>)"
     type        = string
 }
 
-variable "" {
-  
+variable "vpc_cidr"  {
+    description = "Cidr of the VPC to create"
+    type        = string
 }
 
+variable "vpc_enable_dns_hostnames" {
+    descrpition = "Enable DNS hostnames in the VPC"
+    type        = bool
+    default     = true
+}
+
+variable "vpc_enable_vpn_gateway" {
+    descrpition = "Provison a new VPN Gateway resource and attach it to the VPC"
+    type        = bool
+    default     = false
+}
+
+variable "vpc_enable_nat_gateway" {
+    description = "Provision NAT Gateways for each of your private networks"
+    type        = bool
+    default     = true
+}
+
+variable "vpc_single_nat_gateway" {
+    description = "Provision a single shared NAT Gateway across all of your private networks"
+    type        = bool
+    default     = false
+}
+
+variable "vpc_one_nat_gateway_per_az" {
+    description = "Provision only one NAT Gateway per availability zone"
+    type        = bool
+    default     = true
+}
+
+variable "vpc_availability_zones" {
+    description = "Availability Zone of the VPC to create"
+    type        = map(list(string))
+}
+
+variable "vpc_database_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Database"
+    type        = map(list(string))
+}
+
+variable "vpc_database_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Database"
+    type        = string
+    default     = null
+}
+
+variable "vpc_create_database_subnet_group" {
+    description = "Subnet Group of the VPC to create: Database"
+    type        = string
+    default     = true
+}
+
+variable "vpc_elasticache_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Elasticache"
+    type        = map(list(string))
+}
+
+variable "vpc_elasticache_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Elasticache"
+    type        = string
+    default     = null
+}
+
+variable "vpc_create_elasticache_subnet_group" {
+    description = "Subnet Group of the VPC to create: Elasticache"
+    type        = string
+    default     = true
+}
+
+variable "vpc_intra_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Intra (Transit Gateway)"
+    type        = map(list(string))
+}
+
+variable "vpc_intra_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Intra (Transit Gateway)"
+    type        = string
+    default     = null
+}
+
+variable "vpc_private_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Private"
+    type        = map(list(string))
+}
+
+variable "vpc_private_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Private"
+    type        = string
+    default     = null
+}
+
+variable "vpc_public_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Public"
+    type        = map(list(string))
+}
+
+variable "vpc_public_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Private"
+    type        = string
+    default     = null
+}
+  
+variable "vpc_redshift_subnet_cidr" {
+    description = "Subnet CIDR(s) of the VPC to create: Redshift"
+    type        = map(list(string))
+}
+
+variable "vpc_redshift_subnet_suffix" {
+    description = "Subnet Suffix of the VPC to create: Redshift"
+    type        = string
+    default     = null
+}
+
+variable "vpc_create_redshift_subnet_group" {
+    description = "Subnet Group of the VPC to create: Redshift"
+    type        = string
+    default     = true
+} 
 
 #
 # Tag Variables
 #
-variable "namespace" {
-    description = "Value for Resource Tag: Namespace"
+variable "provisioner" {
+    description = "Value for Resource Tag: provisioner"
     type        = string
+    default     = "terraform"
 }
