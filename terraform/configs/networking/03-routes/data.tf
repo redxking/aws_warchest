08102acaf81cb433ea004e76c83c2ebc446a4d7e
Zach Dobrenen <zdobrenen@thedevteam.dev>
2022-09-28 03:28:27 -0700

    working on network routes module

--- terraform/configs/networking/03-routes/data.tf
+++ terraform/configs/networking/03-routes/data.tf
@@ -1,50 +1,10 @@
-# Define any data sources here
-
-data "aws_ec2_transit_gateway" "stno" {
-    id = var.transit_gateway_id[var.region]
+#
+# Data: SSM Parameters - VPC
+#
+data "aws_ssm_parameter" "intra_route_table_ids" {
+  name   = "/infra/${var.environment}/networking/intra_route_table_ids"
 }
 
-data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = module.vpc.vpc_id
-}
-
-data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
-  statement {
-    effect    = "Deny"
-    actions   = ["dynamodb:*"]
-    resources = ["*"]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:sourceVpce"
-
-      values = [module.vpc.vpc_id]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "generic_endpoint_policy" {
-  statement {
-    effect    = "Deny"
-    actions   = ["*"]
-    resources = ["*"]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:SourceVpc"
-
-      values = [module.vpc.vpc_id]
-    }
-  }
+data "aws_ssm_parameter" "private_route_table_ids" {
+  name   = "/infra/${var.environment}/networking/private_route_table_ids"
 }
