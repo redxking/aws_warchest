d5bf06daa1017e1d65deb7bb0b66e060509b8f69
Zach Dobrenen <zdobrenen@thedevteam.dev>
2022-09-28 02:16:16 -0700

    implemented parameters.tf for network sgs

--- terraform/configs/networking/02-security-groups/outputs.tf
+++ terraform/configs/networking/02-security-groups/outputs.tf
@@ -29,7 +29,7 @@ output "transit_gateway_security_group_name" {
 
 output "transit_gateway_security_group_description" {
   description = "The description of the security group (Transit Gateway)"
-  value       = module.routes3_resolver_sg.security_group_description 
+  value       = module.transit_gateway_sg.security_group_description 
 }
 
 output "systems_manager_security_group_arn" {
@@ -63,35 +63,35 @@ output "systems_manager_security_group_description" {
   value       = module.systems_manager_sg.security_group_description 
 }
 
-output "routes3_resolver_security_group_arn" {
+output "route53_resolver_security_group_arn" {
   description = "The ARN of the security group (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_arn 
+  value       = module.route53_resolver_sg.security_group_arn 
 }
 
-output "routes3_resolver_security_group_id" {
+output "route53_resolver_security_group_id" {
   description = "The ID of the security group (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_id
+  value       = module.route53_resolver_sg.security_group_id
 }
 
-output "routes3_resolver_security_group_vpc_id" {
+output "route53_resolver_security_group_vpc_id" {
   description = "The VPC ID (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_vpc_id 
+  value       = module.route53_resolver_sg.security_group_vpc_id 
   sensitive   = true
 }
 
-output "routes3_resolver_security_group_owner_id" {
+output "route53_resolver_security_group_owner_id" {
   description = "The owner ID (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_owner_id 
+  value       = module.route53_resolver_sg.security_group_owner_id 
 }
 
-output "routes3_resolver_security_group_name" {
+output "route53_resolver_security_group_name" {
   description = "The name of the security group (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_name 
+  value       = module.route53_resolver_sg.security_group_name 
 }
 
-output "routes3_resolver_security_group_description" {
+output "route53_resolver_security_group_description" {
   description = "The description of the security group (Route53 Resolver)"
-  value       = module.routes3_resolver_sg.security_group_description 
+  value       = module.route53_resolver_sg.security_group_description 
 }
 
 #
