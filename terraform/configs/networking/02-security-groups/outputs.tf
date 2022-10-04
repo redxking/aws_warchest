589b8bfd67d69f52151ee6bf0a7e55482709e7b7
Zach Dobrenen <zdobrenen@thedevteam.dev>
2022-10-04 01:19:53 -0700

    working to resolve tf state and destroy resources

--- terraform/configs/networking/02-security-groups/outputs.tf
+++ terraform/configs/networking/02-security-groups/outputs.tf
@@ -3,28 +3,28 @@
 #
 output "transit_gateway_security_group_arn" {
   description = "The ARN of the security group (Transit Gateway)"
-  value       = module.transit_gateway_sg.security_group_arn.value
+  value       = module.transit_gateway_sg.security_group_arn
 }
 
 output "transit_gateway_security_group_id" {
   description = "The ID of the security group (Transit Gateway)"
-  value       = module.transit_gateway_sg.security_group_id.value
+  value       = module.transit_gateway_sg.security_group_id
 }
 
 output "transit_gateway_security_group_vpc_id" {
   description = "The VPC ID (Transit Gateway)"
-  value       = module.transit_gateway_sg.security_group_vpc_id.value
+  value       = module.transit_gateway_sg.security_group_vpc_id
   sensitive   = true
 }
 
 output "transit_gateway_security_group_owner_id" {
   description = "The owner ID (Transit Gateway)"
-  value       = module.transit_gateway_sg.security_group_owner_id.value 
+  value       = module.transit_gateway_sg.security_group_owner_id 
 }
 
 output "transit_gateway_security_group_name" {
   description = "The name of the security group (Transit Gateway)"
-  value       = module.transit_gateway_sg.security_group_name.value 
+  value       = module.transit_gateway_sg.security_group_name
 }
 
 output "transit_gateway_security_group_description" {
@@ -34,28 +34,28 @@ output "transit_gateway_security_group_description" {
 
 output "systems_manager_security_group_arn" {
   description = "The ARN of the security group (Systems Manager)"
-  value       = module.systems_manager_sg.security_group_arn.value 
+  value       = module.systems_manager_sg.security_group_arn 
 }
 
 output "systems_manager_security_group_id" {
   description = "The ID of the security group (Systems Manager)"
-  value       = module.systems_manager_sg.security_group_id.value
+  value       = module.systems_manager_sg.security_group_id
 }
 
 output "systems_manager_security_group_vpc_id" {
   description = "The VPC ID (Systems Manager)"
-  value       = module.systems_manager_sg.security_group_vpc_id.value 
+  value       = module.systems_manager_sg.security_group_vpc_id 
   sensitive   = true
 }
 
 output "systems_manager_security_group_owner_id" {
   description = "The owner ID (Systems Manager)"
-  value       = module.systems_manager_sg.security_group_owner_id.value 
+  value       = module.systems_manager_sg.security_group_owner_id
 }
 
 output "systems_manager_security_group_name" {
   description = "The name of the security group (Systems Manager)"
-  value       = module.systems_manager_sg.security_group_name.value
+  value       = module.systems_manager_sg.security_group_name
 }
 
 output "systems_manager_security_group_description" {
@@ -65,7 +65,7 @@ output "systems_manager_security_group_description" {
 
 output "route53_resolver_security_group_arn" {
   description = "The ARN of the security group (Route53 Resolver)"
-  value       = module.route53_resolver_sg.security_group_arn.value
+  value       = module.route53_resolver_sg.security_group_arn
 }
 
 output "route53_resolver_security_group_id" {
@@ -75,13 +75,13 @@ output "route53_resolver_security_group_id" {
 
 output "route53_resolver_security_group_vpc_id" {
   description = "The VPC ID (Route53 Resolver)"
-  value       = module.route53_resolver_sg.security_group_vpc_id.value 
+  value       = module.route53_resolver_sg.security_group_vpc_id 
   sensitive   = true
 }
 
 output "route53_resolver_security_group_owner_id" {
   description = "The owner ID (Route53 Resolver)"
-  value       = module.route53_resolver_sg.security_group_owner_id.value
+  value       = module.route53_resolver_sg.security_group_owner_id
 }
 
 output "route53_resolver_security_group_name" {
