a425a032e35943fede030450029f5dce8ef79271
Zach Dobrenen <zdobrenen@thedevteam.dev>
2022-10-21 02:26:52 -0700

    add ssm parameter resources to tf iam module

---
+++ terraform/configs/iam/01-cross-account-roles/parameters.tf
@@ -0,0 +1,126 @@
+################################################################################
+# SSM Parameters: Cross Account Automation Lambda Role
+################################################################################
+resource "aws_ssm_parameter" "cross_acct_automation_lambda_role_arn" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_lambda_role_resources ? 1 : 0
+  
+  description = "The Arn of the Cross Account Automation Lamnda IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_lambda_role_arn"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_lambda.arn
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_lambda]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_lambda_role_id" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_lambda_role_resources ? 1 : 0
+  
+  description = "The Id of the Cross Account Automation Lamnda IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_lambda_role_id"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_lambda.id
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_lambda]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_lambda_role_name" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_lambda_role_resources ? 1 : 0
+  
+  description = "The Name of the Cross Account Automation Lamnda IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_lambda_role_name"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_lambda.name
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_lambda]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_lambda_role_unique_id" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_lambda_role_resources ? 1 : 0
+  
+  description = "The Unique Id of the Cross Account Automation Lamnda IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_lambda_role_unique_id"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_lambda.unique_id
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_lambda]
+}
+
+################################################################################
+# SSM Parameter: Cross Account Automation Execution Role
+################################################################################
+resource "aws_ssm_parameter" "cross_acct_automation_exec_role_arn" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_exec_role_resources ? 1 : 0
+  
+  description = "The Arn of the Cross Account Automation Execution IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_exec_role_arn"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_exec.arn
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_exec]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_exec_role_id" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_exec_role_resources ? 1 : 0
+  
+  description = "The Id of the Cross Account Automation Execution IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_exec_role_id"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_exec.id
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_exec]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_exec_role_name" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_exec_role_resources ? 1 : 0
+  
+  description = "The Name of the Cross Account Automation Execution IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_exec_role_name"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_exec.name
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_exec]
+}
+
+resource "aws_ssm_parameter" "cross_acct_automation_exec_role_unique_id" {
+  count = local.creatcross_acct_automation_lambdae_related_cross_account_automation_exec_role_resources ? 1 : 0
+  
+  description = "The Unique Id of the Cross Account Automation Execution IAM Role"
+  name        = "/infra/${var.environment}/iam/cross_acct_automation_exec_role_unique_id"
+  type        = "String"
+  value       = aws_iam_role.cross_acct_automation_exec.unique_id
+
+  # Tag(s)
+  tags        = local.tags 
+
+  # Dependency(s)
+  depends_on  = [aws_iam_policy.cross_acct_automation_exec]
+}
+
